612ef0ebfbf8220540c0ceeb58e03027b577aee6
Jason Hickey <jasonh@gmail.com>
2026-08-29 21:45:43 -0700

    f3base

---
+++ c.lean
@@ -0,0 +1,12 @@
+theorem f1 : True := trivial
+theorem f2 : True := trivial
+theorem f3 : True := trivial
+theorem f4 : True := trivial
+theorem f5 : True := trivial
+theorem f6 : True := trivial
+#audit_axioms Fix.f1
+  Fix.f2
+  Fix.f3
+  Fix.f4
+  Fix.f5
+  Fix.f6
